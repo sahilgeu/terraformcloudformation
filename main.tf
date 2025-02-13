c5c57a8a9cb08629c22297e3e9fe52d9d2137625
SAHIL JAIN <107899842+sahilgeu@users.noreply.github.com>
2025-02-13 13:30:45 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_cloudformation_stack" "cloud_migration_factory" {
+  name         = "CloudMigrationFactory"
+  template_url = "https://testingcloudformation.s3.ap-south-1.amazonaws.com/cmf.yaml.yaml"
+
+  capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+
+  parameters = {
+    Application                       = var.application_name
+    Environment                       = var.environment_name
+    Tracker                           = var.migration_tracker
+    ReplatformEC2                     = var.replatform_ec2
+    ServiceAccountEmail               = var.service_account_email
+    CognitoFederation                 = var.allow_additional_identity_provider
+    DeploymentType                    = var.deployment_type
+    WebURL                            = var.full_ui_url
+    EndpointVPCId                     = var.vpc_id
+    PrivateEndpointSubnets            = join(",", var.subnet_ids)
+    SourceCIDR = var.deployment_type == "Public with AWS WAF" ? var.allowed_cidr : ""
+  }
+}
